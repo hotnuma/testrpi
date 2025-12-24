c404893b34d545858c6b769b36bffcacf2f06646
hotnuma <hotnuma@gmail.com>
2025-12-24 08:09:45 +0100

    cleanup

--- sh1106/main.c
+++ sh1106/main.c
@@ -15,12 +15,7 @@ int main()
                      0, 0);
 
     if (ret == OLED_NOT_FOUND)
-    {
-        printf("Unable to initialize I2C bus 0-2,"
-               " please check your connections and verify"
-               " the device address by typing 'i2cdetect -y <channel>\n");
         return 1;
-    }
     
     oled_set_backbuffer(&oled, ucBackBuf);
     
@@ -29,14 +24,6 @@ int main()
 
     oled_string_write(&oled, 0, 0, 0, "Line 1", FONT_NORMAL, 0, 1);
     oled_string_write(&oled, 0, 0, 1, "SS_OLED Library!", FONT_NORMAL, 0, 1);
-    //oled_string_write(&oled, 0, 3, 2, "BIG!", FONT_LARGE, 0, 1);
-    //oled_string_write(&oled, 0, 0, 5, "Small", FONT_SMALL, 0, 1);
-    
-    // for (int i = 0; i < 64; ++i)
-    // {
-    //     oled_set_pixel(&oled, i, 16+i, 1, 1);
-    //     oled_set_pixel(&oled, 127-i, 16+i, 1, 1);
-    // }
     
     printf("Press ENTER to quit\n");
     getchar();
